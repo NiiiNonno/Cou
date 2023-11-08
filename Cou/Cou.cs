5d385319ad0caab8940db591afa552036e399ded
JSACL <j.s.and.c.lover@gmail.com>
2023-11-08 14:13:40 +0900

    プロジェクト ファイルを追加します。

---
+++ Cou/Cou.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nonno.Cou;
+public readonly struct Cou<L, R>
+{
+    readonly L _l;
+    readonly R _r;
+
+    public Cou(L l, R r)
+    {
+        _l = l;
+        _r = r;
+    }
+
+    public void Deconstruct(out L l, out R r)
+    {
+        l = _l;
+        r = _r;
+    }
+}
